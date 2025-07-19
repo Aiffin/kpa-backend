0b157f6e54c31520d348285a8bf443bc6ef6c327
Aiffin <aiffinsk@gmail.com>
2025-07-19 16:19:47 +0530

    validation for fields

--- kpa/models.py
+++ kpa/models.py
@@ -5,15 +5,10 @@ from django.db.models import JSONField
 # Create your models here.
 
 class WheelSpecification(models.Model):
-    formNumber=models.CharField(max_length=50)
+    formNumber=models.CharField(max_length=50,unique=True)
     submittedBy=models.CharField(max_length=50)
     submittedDate=models.DateField()
     fields          = JSONField(blank=True, null=True)
 
     def __str__(self):
         return self.formNumber
-
-# class BogieChecksheet(models.model):
-#     WheelSpecification=models.ForeignKey(WheelSpecification,on_delete=models.CASCADE,related_name="wheel")
-#     inspectionBy=models.CharField(max_length=50)
-
--- kpa/serializers.py
+++ kpa/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import WheelSpecification
-
+import re
 class WheelSerializer(serializers.ModelSerializer):
     class Meta:
         model=WheelSpecification
         fields='__all__'
+
+
+    def validate_formNumber(self, value):
+        pattern = r'^wheel-\d{4}-\d{3}$'
+        if not re.match(pattern, value):
+            raise serializers.ValidationError("formNumber must be in format 'wheel-YYYY-NNN'")
+        return value
+    
+    def validate_submittedBy(self,value):
+        pattern=r'^user_id_\d{3}$'
+        if not re.match(pattern, value):
+            raise serializers.ValidationError("submittedBy must be in format 'user-id-NNN'")
+        return value
+
